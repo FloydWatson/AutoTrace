f8a47679e13f60bb50815ca9cba5bb0baea62926
Travis <travis.falkenberg141@gmail.com>
2020-08-15 17:53:15 +1200

    initial commit

--- auto-trace-web-app/src/main.js
+++ auto-trace-web-app/src/main.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store/store";
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
@@ -12,4 +12,4 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
