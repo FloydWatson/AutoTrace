2daf0b90a613f9b09e2cadd08a9558b91c8b739f
Travis <travis.falkenberg141@gmail.com>
2020-08-15 18:29:09 +1200

    Merged map with nav

--- auto-trace-web-app/src/components/map.vue
+++ auto-trace-web-app/src/components/map.vue
@@ -18,7 +18,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped"  attribute to limit CSS to this component only -->
 <style scoped>
 .map-container {
   height: 80vh;
