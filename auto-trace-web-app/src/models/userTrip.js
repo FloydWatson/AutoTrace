68b9bb8919d2c47c6a5813b023436a80a322ed03
FloydWatson <floydbcseit@gmail.com>
2020-08-15 19:26:07 +1200

    data fix

--- auto-trace-web-app/src/models/userTrip.js
+++ auto-trace-web-app/src/models/userTrip.js
@@ -1,11 +1,11 @@
 export default  class UserTrip {
-    constructor(id, userId, startStopId, endStopId, startTime, endTime, routeTime){
+    constructor(id, userId, startStopId, endStopId, startTime, endTime, routeId){
         this.id = id;
         this.userId = userId;
         this.startStopId = startStopId;
         this.endStopId = endStopId;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.routeTime = routeTime;
+        this.routeId = routeId;
     }
 }
