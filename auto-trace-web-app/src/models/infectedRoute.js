1f70ae4577d9a4ee19b3c0c69a9de16283b7de78
Jesse Gray <jesse.gray@re-leased.com>
2020-08-15 22:03:25 +1200

    added infected routes data

---
+++ auto-trace-web-app/src/models/infectedRoute.js
@@ -0,0 +1,8 @@
+﻿export default class InfectedRoute {
+    constructor(id, name, start, end){
+        this.id = id;
+        this.name = name;
+        this.start = start;
+        this.end = end;
+    }
+}
