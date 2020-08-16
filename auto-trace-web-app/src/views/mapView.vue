d81791a13fc306850e596999d66877f23eb0fda7
Travis <travis.falkenberg141@gmail.com>
2020-08-16 13:24:32 +1200

    Fixed map to include markers with info windows

--- auto-trace-web-app/src/views/mapView.vue
+++ auto-trace-web-app/src/views/mapView.vue
@@ -1,32 +1,28 @@
 <template>
   <div>
-    <!-- <h2>HOME</h2> -->
-    {{ allDummyData }}
     <google-map></google-map>
-    <hello-world :msg="'TEST'"></hello-world>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import helloWorld from '../components/HelloWorld.vue';
 import GoogleMap from '../components/map.vue';
 export default {
   components: {
-    helloWorld,
     GoogleMap,
   },
   data() {
     return {};
   },
   computed: {
-    ...mapGetters(["allRoutes", "allStops", "allUsers", "allUserTrips", "allInfectedRoutes"]),
-    
+    ...mapGetters([
+      'allRoutes',
+      'allStops',
+      'allUsers',
+      'allUserTrips',
+      'allInfectedRoutes',
+    ]),
   },
-  methods: {
-    
-  },
-  methods: {},
   methods: {},
 };
 </script>
